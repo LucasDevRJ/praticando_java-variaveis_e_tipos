6a402dfe7c5f9a00aeda1cbf4504cef1e58c9bc1
Lucas <lucaspereiradelima2020@gmail.com>
2026-02-22 15:00:08 -0300

    Variáveis com seus tipos de dados.

--- src/com/github/lucasdevrj/principal/Principal.java
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -1,4 +1,8 @@
 package com.github.lucasdevrj.principal;
 
 public class Principal {
+    public static void main(String[] args) {
+        int quantidadeDePassos = 500;
+        double alturaEmMetros = 1.60;
+    }
 }
