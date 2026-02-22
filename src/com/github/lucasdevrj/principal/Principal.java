89283768c6845f496ac36d2d3a2b712f1ff3f744
Lucas <lucaspereiradelima2020@gmail.com>
2026-02-22 15:43:04 -0300

    Conversão de tipos de variáveis e escopo local.

--- src/com/github/lucasdevrj/principal/Principal.java
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -2,7 +2,25 @@ package com.github.lucasdevrj.principal;
 
 public class Principal {
     public static void main(String[] args) {
+        //Variáveis e seus tipos de dados
         int quantidadeDePassos = 500;
         double alturaEmMetros = 1.60;
+        String nome = "Lucas";
+
+        //Conversão de tipos de variáveis (type casting)
+        int alturaEmCentimetros = (int) (alturaEmMetros * 100);
+        System.out.println("Altura em centimetros = " + alturaEmCentimetros + "cm.");
+
+        alturaEmMetros  = alturaEmCentimetros / 100.0;
+        System.out.println("Altura em metros = " + alturaEmMetros + "m.");
+
+        int sugestaoDeQuantidadeDePassos = 1000;
+        if (quantidadeDePassos < sugestaoDeQuantidadeDePassos) {
+            String sugestao = "Sugiro que você tente aumentar a meta.";
+            System.out.println(sugestao);
+        }
+
+        Paciente paciente1 = new Paciente();
+
     }
 }
